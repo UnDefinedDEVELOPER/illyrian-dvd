8fef530036a4923bc5c904f3eb391c573545cedb
UnDefinedDEVELOPER <muhammedhalilovic123@gmail.com>
2023-09-10 12:06:26 +0200

    code transfer

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,117 +1,157 @@
 <script lang="ts">
-    let width: number;
-    let showMenu = false;
-    let activePage: any;
+  import { onMount } from "svelte";
+
+  let width: number;
+  let showMenu = false;
+  let activePage: any;
+  export let color: string;
 </script>
 
 <svelte:window bind:innerWidth={width} />
 
 <header
-    style={showMenu
-        ? "height: 100%; background-color: #161411"
-        : "background-color: linear-gradient(rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 100%);"}
+  style="{showMenu
+    ? 'height: 100%; background-color: #161411;'
+    : 'background-color: linear-gradient(rgba(0, 0, 0, 1) 40%, rgba(0, 0, 0, 0) 100%); '}
+        --color: {color};"
 >
+  <!-- <div class="title"> -->
+  {#if width > 767}
     <h1><a href="/">ILLYRIAN</a></h1>
-    {#if width > 767}
-        <nav>
-            <a
-                class={activePage == "development" ? "bolder" : ""}
-                href="/development">DEV</a
-            >
-            <a class={activePage == "video" ? "bolder" : ""} href="/video"
-                >VIDEO</a
-            >
-            <a class={activePage == "design" ? "bolder" : ""} href="/design"
-                >DESIGN</a
-            >
-        </nav>
-        <div class="static-nav">
-            <a href="#about-card">ABOUT</a>
-            <a href="#contact-card">CONTACT</a>
-        </div>
-    {:else}
-        <button
-            on:click={() => {
-                showMenu = !showMenu;
-            }}
+    <nav>
+      <a class={activePage == "development" ? "bolder" : ""} href="/development"
+        >DEV</a
+      >
+      <a class={activePage == "video" ? "bolder" : ""} href="/video">VIDEO</a>
+      <a class={activePage == "design" ? "bolder" : ""} href="/design">DESIGN</a
+      >
+    </nav>
+    <div class="static-nav">
+      <a href="#about-card">ABOUT</a>
+      <a href="#contact-card">CONTACT</a>
+    </div>
+  {:else}
+    <div
+      style={showMenu
+        ? "justify-content: center; gap: 2rem;"
+        : "justify-content: space-between;"}
+      class="title flex w-full"
+    >
+      <h1><a href="/">ILLYRIAN</a></h1>
+      <button
+        on:click={() => {
+          showMenu = !showMenu;
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          style="fill: {color};"
+          ><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+            d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+          /></svg
         >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"
-                ><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-                    d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
-                /></svg
-            >
-        </button>
+      </button>
+    </div>
+    {#if showMenu}
+      <nav class="w-full flex justify-center gap-8">
+        <a
+          class={activePage == "development" ? "bolder" : ""}
+          href="/development">DEV</a
+        >
+        <a class={activePage == "video" ? "bolder" : ""} href="/video">VIDEO</a>
+        <a class={activePage == "design" ? "bolder" : ""} href="/design"
+          >DESIGN</a
+        >
+      </nav>
+      <div class="static-nav w-full flex justify-center gap-8">
+        <a href="#about-card">ABOUT</a>
+        <a href="#contact-card">CONTACT</a>
+      </div>
     {/if}
-
-    <div class="blur" style={showMenu ? "backdrop-filter: blur(40px);" : ""} />
+  {/if}
+  <!-- </div> -->
+  <div class="blur" style={showMenu ? "backdrop-filter: blur(40px);" : ""} />
 </header>
 
 <style lang="postcss">
-    header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        height: 60px;
-        width: 100%;
-        margin: auto;
-        padding: 1rem 5% 1rem 5%;
-        position: absolute;
-        color: #fff;
-        z-index: 100;
-        overflow: hidden;
-        transition: 0.4s all ease-in-out;
+  header {
+    /* --color: #fff; */
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 60px;
+    width: 100%;
+    margin: auto;
+    padding: 1rem 5% 1rem 5%;
+    position: absolute;
+    color: var(--color);
+    z-index: 100;
+    overflow: hidden;
+    transition: 0.4s all ease-in-out;
 
-        @media (min-width: 1440px) {
-            height: 80px;
-        }
+    @media (min-width: 1440px) {
+      height: 80px;
+    }
 
-        .blur {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            backdrop-filter: blur(70px);
-        }
+    @media (max-width: 768px) {
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 2rem;
+    }
 
-        /* h1,
+    .title {
+      z-index: 100;
+    }
+
+    .blur {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      backdrop-filter: blur(70px);
+    }
+
+    /* h1,
         a {
             text-shadow: 1px 1px 5px #000, 1px 1px 5px #000, 1px 1px 5px #000;
         } */
 
-        h1 {
-            font-family: "Righteous", sans-serif;
-            font-size: 2rem;
-            font-weight: 400;
-            z-index: 200;
-        }
-
-        button {
-            z-index: 200;
-            svg {
-                height: 25px;
-                width: auto;
-                fill: #fff;
-            }
-        }
-
-        nav,
-        .static-nav {
-            display: flex;
-            gap: 1.5rem;
-            z-index: 200;
-
-            a {
-                font-size: 1.25rem;
-                font-weight: 300;
-                transition: 0.25s all ease-in;
-                /* background-color: var(--primary); */
-
-                &:hover {
-                    color: var(--primary);
-                    background-color: #fff;
-                }
-            }
-        }
+    h1 {
+      font-family: "Righteous", sans-serif;
+      font-size: 2rem;
+      font-weight: 400;
+      z-index: 200;
     }
+
+    button {
+      z-index: 200;
+      svg {
+        height: 25px;
+        width: auto;
+        fill: #fff;
+      }
+    }
+
+    nav,
+    .static-nav {
+      display: flex;
+      gap: 1.5rem;
+      z-index: 200;
+
+      a {
+        font-size: 1.25rem;
+        font-weight: 300;
+        transition: 0.25s all ease-in;
+        /* background-color: var(--primary); */
+
+        &:hover {
+          color: var(--primary);
+          background-color: #fff;
+        }
+      }
+    }
+  }
 </style>
---
+++ src/components/client/CaroNav.svelte
@@ -0,0 +1,75 @@
+<script is:inline>
+  const scrollNext = () => {
+    const scroller = document.querySelector(".scroller");
+    const width = scroller.offsetWidth;
+
+    scroller.scrollBy(width, 0);
+  };
+
+  const scrollPrev = () => {
+    const scroller = document.querySelector(".scroller");
+    const width = scroller.offsetWidth * -1;
+
+    scroller.scrollBy(width, 0);
+  };
+</script>
+
+<div class="caro-nav flex gap-4">
+  <button on:click={scrollPrev}
+    ><svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="30"
+      viewBox="0 0 20 30"
+      fill="none"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M15.9179 29.4034L0.700369 16.4403C-0.233457 15.6449 -0.233457 14.3551 0.70037 13.5597L15.9179 0.596611C16.8518 -0.198866 18.3658 -0.198866 19.2996 0.596611C20.2335 1.39209 20.2335 2.68181 19.2996 3.47729L5.7729 15L19.2996 26.5227C20.2335 27.3182 20.2335 28.6079 19.2996 29.4034C18.3658 30.1989 16.8518 30.1989 15.9179 29.4034Z"
+        fill="white"
+      />
+    </svg></button
+  >
+  <button on:click={scrollNext}
+    ><svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="30"
+      viewBox="0 0 20 30"
+      fill="none"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M4.08205 0.596608L19.2996 13.5597C20.2335 14.3551 20.2335 15.6449 19.2996 16.4403L4.08205 29.4034C3.14823 30.1989 1.6342 30.1989 0.700371 29.4034C-0.233455 28.6079 -0.233455 27.3182 0.700371 26.5227L14.2271 15L0.70037 3.47729C-0.233456 2.68181 -0.233456 1.39209 0.70037 0.596608C1.6342 -0.198869 3.14823 -0.198869 4.08205 0.596608Z"
+        fill="white"
+      />
+    </svg></button
+  >
+</div>
+
+<style lang="postcss">
+  button {
+    border-bottom: 5px solid rgba(255, 255, 255, 0);
+    transition: 0.2s all ease-out;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media (max-width: 500px) {
+      height: 60px;
+      width: 50px;
+      svg {
+        width: 15px;
+        height: 25px;
+      }
+    }
+
+    &:hover {
+      border-bottom: 5px solid rgba(255, 255, 255, 1);
+    }
+  }
+</style>
