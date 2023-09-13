c4fde7a29333b952e4f0c8fecf817a12d276cff7
UnDefinedDEVELOPER <muhammedhalilovic123@gmail.com>
2023-09-13 15:21:38 +0200

    adding adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
+import nodejs from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+   integrations: [svelte(), tailwind()],
+   adapter: vercel(),
+   output: 'hybrid',
 });
