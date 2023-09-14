b80f9f3388d823aca073fe2e7e721c30499d054a
UnDefinedDEVELOPER <muhammedhalilovic123@gmail.com>
2023-09-14 20:47:30 +0200

    adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-import nodejs from '@astrojs/node';
 import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
@@ -8,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
    integrations: [svelte(), tailwind()],
-   adapter: vercel(),
    output: 'hybrid',
+   adapter: vercel(),
 });
