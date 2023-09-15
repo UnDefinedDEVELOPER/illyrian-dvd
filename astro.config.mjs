1f7fef7805911c2df30630aecf185d475312439f
UnDefinedDEVELOPER <muhammedhalilovic123@gmail.com>
2023-09-15 20:47:04 +0200

    build option: copies the pdf to dist folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,14 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [svelte(), tailwind()]
+   integrations: [svelte(), tailwind()],
+   site: 'https://illyriandvd.vercel.app',
+   build: {
+      copy: [
+        {
+          from: 'public/CV_eng.pdf', // Replace with the actual path to your PDF file in the public folder
+          to: 'dist/CV_eng.pdf',     // Specify the destination path in the output folder
+        },
+      ],
+    },
 });
